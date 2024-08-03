aa505d99779072970a6e3650793252097c6def54
delawar shah <delawarshah8@gmail.com>
2024-08-03 16:25:06 +0400

    issuse

--- src/app/dashboardWrapper/page.tsx
+++ src/app/dashboardWrapper/page.tsx
@@ -1,17 +1,14 @@
 import React from 'react'
 import Navbar from '../(components)/Navbar'
 
-const DashboardWrapper = ({children}:{children: React.ReactNode}) => {
+const DashboardWrapper = ({ children }: { children: React.ReactNode }) => {
   return (
-    <div className={`light flex  bg-gray-50 w-full min-h-screen`}>
-      Sidebar 
-      <main className={`flex flex-col w-full h-full py-7 px-9 bg-gray-200 md:pl-24`}>
-        <Navbar/>
-      {children}
+    <div className="light flex bg-gray-50 w-full min-h-screen">
+      Sidebar
+      <main className="flex flex-col w-full h-full py-7 px-9 bg-gray-200 md:pl-24">
+        <Navbar />
+        {children}
       </main>
-      
-      
-     
     </div>
   )
 }
