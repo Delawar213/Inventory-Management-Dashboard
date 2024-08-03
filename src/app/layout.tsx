4a6d62990b903aa318d68a38cfae92d54953abb2
delawar shah <delawarshah8@gmail.com>
2024-08-03 16:58:33 +0400

    dashboard

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 import type { Metadata } from 'next';
 import { Inter } from 'next/font/google';
 import './globals.css';
-import DashboardWrapper from '@/dashboardWrapper';
+import DashboardWrapper from '@/app/dashboardWrapper';
 
 
 
