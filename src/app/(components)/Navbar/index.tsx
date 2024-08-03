133bc33e3c7215ea33c4f2372ad737362ed075a6
delawar shah <delawarshah8@gmail.com>
2024-08-03 17:20:44 +0400

    dashboard

--- src/app/(components)/Navbar/index.tsx
+++ src/app/(components)/Navbar/index.tsx
@@ -1,6 +1,6 @@
 "use client"
 import { MenuPaper } from '@mui/material'
-import { Camera, Menu } from 'lucide-react'
+import { Bell, Camera, Menu } from 'lucide-react'
 import React from 'react'
 
 const Navbar = () => {
@@ -11,12 +11,22 @@ const Navbar = () => {
         <button className="px-3 py-3 bg-gray-100 rounded-full hover:bg-blue-100" onClick={()=>{alert("clicked")}}>
           <Menu className="w-4 h-4"/>
         </button>
-        <div className="relative">
-          <input type="search" placeholder="Start to type groups & Product" className="pl-10 pr-14 py-2 w-50 md:w-80 border-2 border-gray-300 bg-white rounded-lg  focus:outline-none focus:border-blue-500"/>
         </div>
+        <div className="relative">
+          <input
+            type="search"
+            placeholder="Start type to search groups & products"
+            className="pl-10 pr-4 py-2 w-50 md:w-60 border-2 border-gray-300 bg-white rounded-lg focus:outline-none focus:border-blue-500"
+          />
+
+          <div className="absolute inset-y-0 left-0 pl-3 flex items-center pointer-events-non">
+            <Bell className="text-gray-500" size={20} />
+          </div>
+        </div>
+
       </div>
-      Navbar 
-    </div>
+     
+  
   )
 }
 
