a7fd8c2346ca3716cc16dc305de3871fa6fcbf59
delawar shah <delawarshah8@gmail.com>
2024-08-07 17:42:07 +0400

    dashboard complete

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -14,8 +14,8 @@ const Dashboard = () => {
     >
       {/* <div className="row-span-3 xl:row-span-6 bg-gray-500"></div> */}
       <CardPopularProducts/>
-      {/* <div className="row-span-3 xl:row-span-6 bg-white shadow-md rounded-2xl"></div> */}
-      <CardSalesSummary/>
+      <div className="row-span-3 xl:row-span-6 bg-white shadow-md rounded-2xl"></div>
+      {/* <CardSalesSummary/> */}
       {/* <div className="row-span-3 xl:row-span-3 col-span-1 md:col-span-2 xl:col-span-1 bg-gray-400"></div> */}
       <CardPurchaseSummary/>
       <CardExpenseSummary />
